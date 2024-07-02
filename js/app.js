8e926131d768428fe1ca35394d293c647e7aef9c
Ahrorbek05 <akramjonovahrorbek@gmail.com>
2024-07-02 10:58:45 +0500

    home work

--- js/app.js
+++ js/app.js
@@ -1,60 +1,79 @@
-const field = document.querySelector('#field');
+const form = document.getElementById('form');
 const button = document.getElementById('button');
-const wrapper = document.querySelector('#todo-wrapper');  
+const name = document.getElementById('name');
+const surname = document.getElementById('surname');
+const age = document.getElementById('age');
+const nationality = document.getElementById('nationality');
+const wrapper = document.getElementById('wrapper');
 
-function createItem(value) {
-    return `
-              <div class="left">
-                   name:<p>${value}</p>
-                   surname:<p>${value}</p>
-                   age:<p>${value}</p>
-              </div>
-    `;
-    }    
+function validate() {
+    if(name.value.length < 3){
+        alert('Name must be more than 3 character');
+        name.focus();
+        name.style.outlineColor = 'red';
+        return false;
 
-    
-button && button.addEventListener('click', function (event) {
+    }
+
+    if(!age.value || age.value > 150 || age.value < 0){
+            alert('Age is not valid');
+            age.focus();
+            age.style.outlineColor = 'red';
+            return false;
+    }
+
+
+
+    return true;
+}
+
+function getUsers() {
+    const users = [];
+    if(localStorage.getItem('users')){
+        users = JSON.parse(localStorage.getItem('users'))
+    }
+    return users;
+}
+
+button && button.addEventListener('click', function(event){
     event.preventDefault();
-    const todo = field.value;
 
-    if(todo.length < 5){
-        alert('Eng kamida 6 ta belgidan iborat bolishi kerak');
-
-        field.focus();
-        field.style.outlineColor = 'red';
+    const isValid = validate();
+    if(!isValid){
         return;
     }
 
-    const item = createItem(todo);
-    wrapper.innerHTML += item;
+    const users = getUsers();
+    const user = {
+        name: name.value,
+        surname: surname.value,
+        age: age.value,
+        nationality: nationality.value,
+        id: Date.now()
+    }
 
-    field.value = '';
-    field.focus();
-    saveItemLocalStroge(field.value);
+    users.push(user);
+    localStorage.setItem('users', JSON.stringify(users));
+    form.reset();
 });
 
-const local = document.querySelector('#local');
-local && local.addEventListener('click', function () {
-     let name = 'Ahrorbek';
-     localStorage.setItem('name', name);
-     localStorage.setItem('age', age);
+function createCard(user) {
+    return `
+    <div class="card">
+    <h3>${user.name + " " + user.surname }</h3>
+    <p>${user.age}</p>
+    <p>${user.nationality}</p>
 
-     const users = [
-          {name: 'john', age: 25},
-          {name: 'doe', age: 35},
-     ];
+    <button data-id = "${user.id}">edit</button>
+    <button data-id = "${user.id}">delete</button>
+</div>
+    `;
+}
 
-     localStorage.setItem('users', JSON.stringify(users))
+document.addEventListener('DOMContentLoaded', function () {
+    let users = getUsers();
+    users.length > 0 && users.forEach(element => {
+        let card = createCard(element)
+        wrapper.innerHTML += card; 
+    });
 })
-local.style.padding = '10px 20px';
-local.style.marginTop = '25px';
-local.style.marginLeft = '25px';
-local.style.borderRadius = '12px';
-local.style.border = 'none';
-local.style.background = 'transparent';
-local.style.border = '1px solid #ababab';
-local.style.cursor = 'pointer';
-local.style.fontSize = '16px';
-local.style.fontWeight = '500';
-local.style.alignItems = 'center';
-local.style.color = 'chocolate';  
